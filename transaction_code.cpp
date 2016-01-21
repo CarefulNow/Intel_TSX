6ef8d88d9087d22dd219e434c4d7268d57e18749
Daniel Dowd <dowdda@tcd.ie>
2016-01-21 15:16:34 +0000

    Base code for using Intels transactional memory

---
+++ transaction_code.cpp
@@ -0,0 +1,27 @@
+unsigned int transaction = 1;                                                                      
+unsigned int status = 0;                                                
+unsigned int attempts = 8;                                              
+while(1) {                                                              
+    if(transaction == 1) {                                              
+        status = _xbegin();                                             
+    } else {                                                            
+        ACQUIRE_LOCK();                                                 
+        status = _XBEGIN_STARTED;                                      
+    }                                                                   
+    if(status == _XBEGIN_STARTED) {                                    
+        if(_xtest() && lock_var) _xabort(0x01);                         
+                                                                      
+			/*UPDATE SHARED DATA STRUCTURE*/
+
+        if(_xtest()) _xend();                                           
+        else RELEASE_LOCK();                                            
+        break;                                                          
+    } else {                                                            
+        if (attempts > 0) {                                             
+            attempts--;                                                 
+        } else transaction = 0;                                        
+        while(lock_var) {                                                           
+            _mm_pause();                                                
+        }                                              
+    }                                                                   
+}  
